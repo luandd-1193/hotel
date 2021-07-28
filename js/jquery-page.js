b5448980a058d57797270a87c5104346eae09ffd
Dang.Dinh.Luan <dang.dinh.luan@framgia.com>
2021-07-28 15:18:32 +0700

    change css

--- js/jquery-page.js
+++ js/jquery-page.js
@@ -8,14 +8,23 @@ jQuery(document).ready(function($){
 	  adaptiveHeight: true,
 	  autoplay: true,
 	});
+
 	$('.slick_follow').slick({
-	  slidesToShow: 2,
+	  slidesToShow: 3,
 	  slidesToScroll: 1,
 	  dots: false,
 	  centerMode: true,
 	  focusOnSelect: false,
 	  arrows: false,
 	  infinite: true,
+	  responsive: [{
+	    breakpoint: 1024,
+	    settings: {
+	      slidesToShow: 2,
+	      slidesToScroll: 1,
+	      centerPadding: '40px',
+	    }
+	  }]
 	});
 });
 
